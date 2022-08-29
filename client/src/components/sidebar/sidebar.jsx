0533720b0233f732428c12905fd72536fd952282
Angelica Moberg <angelica.moberg@hotmail.com>
2022-08-29 15:19:50 +0200

    Create room client
    Fixed initial condition to use create room. Design not fixed yet only functionality

--- client/src/components/sidebar/sidebar.jsx
+++ client/src/components/sidebar/sidebar.jsx
@@ -4,7 +4,7 @@ import ListOfRooms from "./listOfRooms"
 const Sidebar = () => {
 
     return (
-        <div style={{flexGrow: "1", backgroundColor: "red"}}>
+        <div style={{flexGrow: "1", backgroundColor: "#484848"}}>
             <CreateRoom/>
             <ListOfRooms/>
         </div>
