6635582a4e119ab09926ecb076bb13e76c7e0df1
Angelica Moberg <angelica.moberg@hotmail.com>
2022-08-29 14:46:59 +0200

    structure

---
+++ client/src/components/sidebar/listOfRooms.jsx
@@ -0,0 +1,13 @@
+import UsersInRoom from "./usersInRoom"
+
+const ListOfRooms = () => {
+
+    return (
+        <div>
+        Lista med rum
+        <UsersInRoom/>
+        </div>
+    )
+}
+
+export default ListOfRooms
