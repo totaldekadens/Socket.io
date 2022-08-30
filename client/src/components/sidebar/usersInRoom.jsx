e7a2d90a40a0d068496b74708f13e46a36a35bae
Hugo Bengtsson <hugo@capellium.se>
2022-08-30 09:45:39 +0200

    Render out active rooms and connected users.

--- client/src/components/sidebar/usersInRoom.jsx
+++ client/src/components/sidebar/usersInRoom.jsx
@@ -1,11 +1,30 @@
 
-const UsersInRoom = () => {
+const UsersInRoom = (props) => {
 
     return (
         <div>
-       Användare i rum
+            {
+                props.users.map((user) => {
+                    
+                    return(
+                        <p style={{color: "white"}}>{user.nickname}</p>
+                    )
+                })
+            }
+            <div style={joinButton}>
+                <p style={{color: "white"}}>Anslut</p> {/* Use context to check if the room = the room that the user is connected to. And render "leave" instead of "connect". */}
+            </div>
         </div>
     )
+}
+
+const joinButton = {
+    padding: "3px",
+    marginTop: "20px",
+    backgroundColor: "green",
+    width: "70px",
+    textAlign: "center",
+    borderRadius: "5px"
 }
 
 export default UsersInRoom
